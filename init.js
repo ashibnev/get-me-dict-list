bdaa1a5fd56b6db54cffab45c8a7766c02167674
ashibnev <a.shibnev@propellerads.net>
2021-08-06 14:01:06 +0300

    clean code

--- init.js
+++ init.js
@@ -1,17 +1,4 @@
 import { CountryList } from './src/index.js';
 import { config } from './config.js';
 
-// new CountryList({
-//   url: 'https://restcountries.eu/rest/v2/all',
-//   fileName: 'data',
-//   template(_this) {
-//     return {
-//       [_this.alpha2Code]: {
-//         en: _this.name,
-//         [_this.languages[0].iso639_1]: _this.nativeName,
-//       },
-//     };
-//   },
-// });
-
 new CountryList(config);
