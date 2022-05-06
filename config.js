d3a02e4688923c36d30395bbd4513b6a45184c62
ashibnev <a.shibnev@propellerads.net>
2022-05-06 19:09:22 +0400

    fix

--- config.js
+++ config.js
@@ -6,12 +6,14 @@ export const config = {
     const nameInEnglish = _this.name;
     const commonLang = _this.languages[0].iso639_1;
     const nativeName = _this.nativeName;
+    const currencyCode = _this.currencies ? _this.currencies[0].code : 'USD';
 
     return {
       [countryCode]: {
         en: nameInEnglish,
         [commonLang]: nativeName,
-        translations: _this.translations
+        translations: _this.translations,
+        currencyCode: currencyCode
       },
     };
   },
